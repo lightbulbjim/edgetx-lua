f8ca537bf5ebb2e954ea84ddd1c46227060c5b6c
Chris Rendle-Short <chris@killred.net>
2023-12-30 22:12:14 +1100

    Add tmrvar
    This is a function script which configures a timer from a gvar.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # EdgeTX Lua Scripts
 
 This repository contains various [Lua](https://luadoc.edgetx.org) knick knacks for EdgeTX.
+
+
+## tmrvar
+
+This is a run-once function script which configures a timer based on the value of a gvar.
+
+Intended for use in glider contests, it allows quickly switching the timer between preset values using a switch.
+
+Countdown values (gvar > 0) will enable minute calls and a 30 second countdown.
+
+Enabling the count up timer (gvar = 0) will disable all voice callouts, and is intended for general sport flying.
---
+++ SCRIPTS/FUNCTIONS/tmrvar.lua
@@ -0,0 +1,44 @@
+-- Configures a timer based on the value of a gvar
+
+-- Timer to manipulate. Zero indexed.
+local timerIndex = 0
+
+-- GVar used for setting timer values. Zero indexed.
+local timerVar = 8
+
+-- File to play when announcing sport mode.
+local sportFile = "sport.wav"
+
+
+local function init()
+    -- Called once when the model is loaded
+end
+
+local function run(event, touchState)
+    local minutes = model.getGlobalVariable(timerVar, 0)
+
+    if minutes == 0 then  -- Sport mode
+        model.setTimer(timerIndex,
+                {
+                    start = 0,
+                    countdownBeep = 0,
+                    minuteBeep = false,
+                }
+        )
+        playFile(sportFile)
+    elseif minutes > 0 then  -- Task mode
+        model.setTimer(timerIndex,
+                {
+                    start = minutes * 60,
+                    countdownBeep = 2,  -- Voice
+                    minuteBeep = true,
+                }
+        )
+        playNumber(minutes, 0)
+    end
+
+    -- Non-zero return value halts the script. We only need it to run once per invocation.
+    return 1
+end
+
+return { run = run, init = init }
